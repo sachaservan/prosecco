2f5dfb695cdfcca501c859772fc0c87642226054
ezg <emanuel.zgraggen@gmail.com>
2018-03-23 17:34:26 -0400

    update

--- src/Benchmark/results/plots/runtimes2.py
+++ src/Benchmark/results/plots/runtimes2.py
@@ -40,7 +40,7 @@ sns.set(context='paper', style={'axes.axisbelow': True,
     'ytick.color': '.15',
     'ytick.direction': u'out',
     'ytick.major.size': 0.0,
-    'ytick.minor.size': 0.0}, font_scale = 1.3)
+    'ytick.minor.size': 0.0}, font_scale = 2)
 
 flatui = ['#28aad5', '#b24d94', '#38ae97' ,'#ec7545']
 
@@ -55,26 +55,26 @@ prosecco_std = []
 prefixspan_means = []
 prefixspan_std = []
 
-datasets = ['BMS-0.03', 'KORSARAK-0.05', 'SIGN-0.40', 'BIBLE-0.40', 'FIFA-0.30']
+datasets = []
 prefixspan = runtimes['prefixspan']
 prosseco = runtimes['prosecco']
 for ds in prefixspan:
-    if ds in datasets:
+    datasets.append(ds)
 
-        r_ps = prefixspan[ds]
-        
-        prefixspan_means.append(np.array(r_ps).mean())
-        b = np.array(r_ps).std() / math.sqrt(len(r_ps))
-        prefixspan_std.append(1.96 * b)
+    r_ps = prefixspan[ds]
+    
+    prefixspan_means.append(np.array(r_ps).mean())
+    b = np.array(r_ps).std() / math.sqrt(len(r_ps))
+    prefixspan_std.append(1.96 * b)
 
-        r_pr = prosseco[ds]
-        prosecco_means.append(np.array(r_pr).mean())    
-        b = np.array(r_pr).std() / math.sqrt(len(r_pr))
-        prosecco_std.append(1.96 * b)
+    r_pr = (prosseco[ds] - np.array(r_ps).mean()) / np.array(r_ps).mean()
+    prosecco_means.append(np.array(r_pr).mean())    
+    b = np.array(r_pr).std() / math.sqrt(len(r_pr))
+    prosecco_std.append(1.96 * b)
 
 print()
 print(len(prosecco_std), len(prefixspan_std))
-print(datasets)
+print()
 
 
 ind = np.arange(len(prosecco_means))  # the x locations for the groups
@@ -84,47 +84,20 @@ f, (ax) = plt.subplots(1, 1, sharey=False, figsize=(5, 3.5))
 
 rects1 = ax.bar(ind - width/2, prosecco_means, width, yerr=prosecco_std,
                 color=flatui[0], label='ProSecCo')
-rects2 = ax.bar(ind + width/2, prefixspan_means, width, yerr=prefixspan_std,
-                color=flatui[1], label='PrefixSpan', hatch='//')
+#rects2 = ax.bar(ind + width/2, prefixspan_means, width, yerr=prefixspan_std,
+#                color=flatui[1], label='PrefixSpan', hatch='//')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('Runtime (s)')
 ax.set_xticks(ind)
 ax.set_xticklabels(datasets)
 locs, labels = plt.xticks()
-plt.setp(labels, rotation=45)    
+plt.setp(labels, rotation=90)    
 ax.legend(loc='upper right')
-ax.yaxis.set_major_formatter(plt.FuncFormatter(second_formatter))
+#ax.yaxis.set_major_formatter(plt.FuncFormatter(second_formatter))
 ax.legend()
-ax.set_ylim([0.0, 500000])
 
-def autolabel(rects, xpos='center'):
-    """
-    Attach a text label above each bar in *rects*, displaying its height.
-
-    *xpos* indicates which side to place the text w.r.t. the center of
-    the bar. It can be one of the following {'center', 'right', 'left'}.
-    """
-
-    xpos = xpos.lower()  # normalize the case of the parameter
-    ha = {'center': 'center', 'right': 'left', 'left': 'right'}
-    offset = {'center': 0.5, 'right': 0.57, 'left': 0.43}  # x_txt = x + w*off
-
-    newlist = sorted(rects, key=lambda x: x.get_x() , reverse=False)
-
-
-    i = 0
-    for rect in newlist:
-        print(rect.get_x() )
-        height = rect.get_height()
-        text = np.mean(prosseco[datasets[i]]) / np.mean(prefixspan[datasets[i]])
-        ax.text(rect.get_x() + rect.get_width()*offset[xpos], 1.05*height,
-                '{:.2f}'.format(text) + 'x', ha=ha[xpos], va='bottom', fontsize=12)
-        i += 1
-
-
-autolabel(rects1, "center")
-
-f.savefig('../fig/' + 'runtime.pdf', bbox_inches='tight')
+f.savefig('../fig/' + 'runtime2.pdf', bbox_inches='tight')
 plt.tight_layout()
+
 plt.show()
