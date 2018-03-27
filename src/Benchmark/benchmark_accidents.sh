9c538568adb47eb37cc95a702357865253418677
ezg <emanuel.zgraggen@gmail.com>
2018-03-26 21:02:29 -0400

    updated results and benchmark script

--- src/Benchmark/benchmark_accidents.sh
+++ src/Benchmark/benchmark_accidents.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cd server
-dotnet run -c Release -- -p 50003 &
+dotnet run -c Release -- -p 50030 &
 
 sleep 15
 
 
 cd ../client
 
-dotnet run -c Release -- -p 50003 -f ~/data/sequence/seq-kosarak-lg.txt -s 0.05 -z 20000 -e 0.05 -a PrefixSpan -r 5 -d 1249951 -i ps-seq-kosarak-lg-0.05
-dotnet run -c Release -- -p 50003 -f ~/data/sequence/seq-kosarak-lg.txt -s 0.05 -z 20000 -e 0.05 -a ProSecCo -r 5 -d 1249951 -i u_ips-seq-kosarak-lg-0.05
+dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.50 -z 20000 -e 0.05 -a PrefixSpan -r 5 -d 1249951 -i ps-seq-accidents-lg-0.50
+dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.50 -z 20000 -e 0.05 -a ProSecCo -r 5 -d 1249951 -i u_ips-seq-accidents-lg-0.50
 
-dotnet run -c Release -- -p 50003 -f ~/data/sequence/seq-kosarak-lg.txt -s 0.10 -z 20000 -e 0.05 -a PrefixSpan -r 5 -d 1249951 -i ps-seq-kosarak-lg-0.10
-dotnet run -c Release -- -p 50003 -f ~/data/sequence/seq-kosarak-lg.txt -s 0.10 -z 20000 -e 0.05 -a ProSecCo -r 5 -d 1249951 -i u_ips-seq-kosarak-lg-0.10
+dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.60 -z 20000 -e 0.05 -a PrefixSpan -r 5 -d 1249951 -i ps-seq-accidents-lg-0.60
+dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.60 -z 20000 -e 0.05 -a ProSecCo -r 5 -d 1249951 -i u_ips-seq-accidents-lg-0.60
 
-dotnet run -c Release -- -p 50003 -f ~/data/sequence/seq-kosarak-lg.txt -s 0.15 -z 20000 -e 0.05 -a PrefixSpan -r 5 -d 1249951 -i ps-seq-kosarak-lg-0.15
-dotnet run -c Release -- -p 50003 -f ~/data/sequence/seq-kosarak-lg.txt -s 0.15 -z 20000 -e 0.05 -a ProSecCo -r 5 -d 1249951 -i u_ips-seq-kosarak-lg-0.15
+dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.70 -z 20000 -e 0.05 -a PrefixSpan -r 5 -d 1249951 -i ps-seq-accidents-lg-0.70
+dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.70 -z 20000 -e 0.05 -a ProSecCo -r 5 -d 1249951 -i u_ips-seq-accidents-lg-0.70
