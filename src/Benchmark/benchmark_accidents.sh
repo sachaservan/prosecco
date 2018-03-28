5b8db21d93bc508b53c914a680cc4f1f006d2e9b
ezg <emanuel.zgraggen@gmail.com>
2018-03-28 12:49:13 -0400

    new results

--- src/Benchmark/benchmark_accidents.sh
+++ src/Benchmark/benchmark_accidents.sh
@@ -7,12 +7,12 @@ sleep 15
 
 cd ../client
 
-dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.80 -z 20000 -e 0.05 -a PrefixSpan -r 5 -d 1700915 -i ps-seq-accidents-lg-0.80
-dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.80 -z 20000 -e 0.05 -a ProSecCo -r 5 -d 1700915 -i u_ips-seq-accidents-lg-0.80
+#dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.80 -z 20000 -e 0.05 -a PrefixSpan -r 5 -d 1700915 -i ps-seq-accidents-lg-0.80
+#dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.80 -z 20000 -e 0.05 -a ProSecCo -r 5 -d 1700915 -i u_ips-seq-accidents-lg-0.80
 
-dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.85 -z 20000 -e 0.05 -a PrefixSpan -r 5 -d 1700915 -i ps-seq-accidents-lg-0.85
-dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.85 -z 20000 -e 0.05 -a ProSecCo -r 5 -d 1700915 -i u_ips-seq-accidents-lg-0.85
+#dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.85 -z 20000 -e 0.05 -a PrefixSpan -r 5 -d 1700915 -i ps-seq-accidents-lg-0.85
+#dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.85 -z 20000 -e 0.05 -a ProSecCo -r 5 -d 1700915 -i u_ips-seq-accidents-lg-0.85
 
 dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.90 -z 20000 -e 0.05 -a PrefixSpan -r 5 -d 1700915 -i ps-seq-accidents-lg-0.90
-dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.90 -z 20000 -e 0.05 -a ProSecCo -r 5 -d 1700915 -i u_ips-seq-accidents-lg-0.90
+#dotnet run -c Release -- -p 50030 -f ~/data/sequence/seq-accidents-lg.txt -s 0.90 -z 20000 -e 0.05 -a ProSecCo -r 5 -d 1700915 -i u_ips-seq-accidents-lg-0.90
 
